7f9a415f363ae2b604a72b4d6fe8de3b12eb4dce
kotaro <kotaro@kotaro-no-MacBook-Air.local>
2015-12-25 03:57:33 -0500

    ###

---
+++ src/Java2dRandom.java
@@ -0,0 +1,28 @@
+import javax.swing.*;
+
+public class Java2dRandom extends JPanel{
+  
+  public static void main(String[] args){
+    JFrame frame = new JFrame();
+
+    Java2dRandom app = new Java2dRandom();
+    frame.getContentPane().add(app);
+
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setBounds(10, 10, 800, 800);
+    frame.setTitle("ランダム可視化");
+    frame.setVisible(true);
+    frame.setResizable(false);
+    
+    PaintOrisinal pa=new PaintOrisinal(frame);
+    for(int i=0;i<100000;i++){
+      pa.paintComponent(frame.getGraphics());
+      try {
+        Thread.sleep(1);
+      } catch (InterruptedException e) {
+          e.printStackTrace();
+      }
+    }
+  }
+    
+}
---
+++ src/PaintOrisinal.java
@@ -0,0 +1,87 @@
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.geom.Line2D;
+import java.util.Arrays;
+import java.util.Random;
+
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class PaintOrisinal extends JPanel{
+  private static int[][] ar={{400,400},{400,400}};
+  private static int old;
+  JFrame frame;
+  public PaintOrisinal() {
+    // TODO Auto-generated constructor stub
+    super();
+    this.render();
+  }
+  public PaintOrisinal(JFrame frame_) {
+    // TODO Auto-generated constructor stub
+    super();
+    frame=frame_;
+    //this.render();
+  }
+  private void render(){
+    frame = new JFrame();
+
+    Java2dRandom app = new Java2dRandom();
+    frame.getContentPane().add(app);
+
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setBounds(10, 10, 800, 800);
+    frame.setTitle("ランダム可視化");
+    frame.setVisible(true);
+  }
+  public void paintComponent(Graphics g){
+    //Graphics2D g = (Graphics2D) frame.getGraphics();
+    
+//    g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, 
+//                        RenderingHints.VALUE_ANTIALIAS_ON);
+    Random rnd = new Random();
+    //for(int i=0;i<10;i++){
+    int ran;
+    do{
+      ran = rnd.nextInt(4);
+      Math.floor(ran);
+    }while(Math.abs(old-ran)==2);//一回前の道を逆戻りしない設定
+    old=ran;
+      this.ando(ran);
+      ((Graphics2D) g).draw(new Line2D.Double(ar[0][0], ar[0][1], ar[1][0], ar[1][1]));
+    //}
+      g.dispose();
+    
+  }
+  public void clear(){
+    frame.repaint();
+  }
+  public void ando(int a){
+    for(int[] xd:ar){
+      for(int x:xd){
+        if(x<0||x>800){
+          Arrays.fill(ar[0], 400);
+          Arrays.fill(ar[1], 400);
+        }
+      }
+    }
+    
+    ar[0][0]=ar[1][0];
+    ar[0][1]=ar[1][1];
+    switch(a){
+      case 0:
+        ar[1][0]-=1;
+        break;
+      case 1:
+        ar[1][1]+=1;
+        break;
+      case 2:
+        ar[1][0]+=1;
+        break;
+      case 3:
+        ar[1][1]-=1;
+        break;
+    }
+  }
+}
